3819bfdae9e314d6f502e551551a91807690c47f
Martynas <martynas@Martynass-MBP.lan>
2022-10-13 19:47:48 +0300

    added server configuaration

---
+++ source/server.js
@@ -0,0 +1,26 @@
+// Serverio konfigūracija
+
+const { admin, authentication, customers} = require('./routes/exports.js');
+const { port } = require('./config.js');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/admin/', admin);
+app.use('/users/', customers);
+app.use('/authentication', authentication);
+
+app.get('/', (request, response) => {
+  response.send('Server is running');
+});
+
+app.all('*', (request, response) => {
+  response.status(400).send({ error: '404 Page Not Found' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}...`);
+});
