ae0eabde2b33f4160933c7d85ad8061f7dfd7052
Martynas <martynas@Martynass-MBP.lan>
2022-10-13 19:49:36 +0300

    module exports for route files

---
+++ source/routes/exports.js
@@ -0,0 +1,5 @@
+const admin = require('./admin');
+const customers = require('./customers');
+const authentication = require('./authentication');
+
+module.exports = { admin, customers, authentication };
